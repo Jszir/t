44f551c8b28a63072b290b85c0644ecfb8ff2d10
Jszir <jassiraabra@gmail.com>
2025-02-14 03:37:11 -0500

    casi finalziando

--- back.js
+++ back.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let y_bt = document.getElementById("yes");
     let n_bt = document.getElementById("no");
     let img_1 = document.getElementById("img1");
-
+    let questio= document.getElementById("question")
     y_bt.style.width = '100px';
     y_bt.style.height = '100px';
     n_bt.style.width = '100px';
@@ -40,8 +40,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     
         
-    const images = ['images/pv.jpg', 'images/tr.jpg', 'images/tr.jpg'];
-    const images2 = ['images/tr.jpg', 'images/tr.jpg', 'images/vt.jpg', 'images/otra.jpg'];
+    const images = ['images/trr.png', 'images/te.png', 'images/tr.jpg'];
+    const images2 = ['images/pv.png', 'images/vm.png', 'images/lg.png', 'images/tr.png', 'images/td.png'];
     let currentIndex = 0;
     img_1.src = images[currentIndex];
 
@@ -55,10 +55,11 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log(`currentIndex: ${currentIndex}`);
         }, 200);
     }
-
+let clicknt_count = 0
     n_bt.addEventListener("click", function() { 
+        clicknt_count++
         // Reiniciar el índice si el botón anterior era diferente
-        if (previousButton !== 1) currentIndex = 0;
+        if (previousButton !== 1) currentIndex = 0, clicknt_count = 0;
         previousButton = 1;
 
         width += 50;
@@ -79,6 +80,33 @@ document.addEventListener("DOMContentLoaded", function () {
         y_bt.style.animation = '';
 
         change_array(images2);
+
+        console.log(clicknt_count)
+
+        if (clicknt_count === 3) { 
+            n_bt.style.display= 'none'
+            y_bt.style.width= '500px'
+            y_bt.style.height= '500px'
+            y_bt.style.backgroundColor= 'fucsia'
+            y_bt.style.color= 'white'
+            document.body.style.backgroundColor= "black"
+            questio.style.fontFamily= '"Eater", serif'
+             questio.style.fontSize= '35px'
+             questio.style.animation = 'shake 0.5s infinite'
+             y_bt.style.animation= 'shake 0.5s infinite'
+
+             document.querySelectorAll('div').forEach(div => {
+                div.style.animation = 'shake 0.5s infinite';
+            });
+
+            document.querySelectorAll('h1').forEach(div => {
+                div.style.animation = 'shake 0.5s infinite';
+            });
+
+            document.querySelectorAll('h2').forEach(div => {
+                div.style.animation = 'shake 0.5s infinite';
+            });
+        }
     });
 
     y_bt.addEventListener("click", function() {
@@ -92,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
         img_1.style.opacity = 0;
         y_bt.style.animation = 'bounce 1s infinite';
 
-        if (y_bt.textContent === 'Sí (dale de nuevo)' && currentIndex === 2) { 
+        if (y_bt.textContent === 'Sí (dale de nuevo)' && currentIndex === 1) { 
             y_bt.style.position = 'fixed'; // Sigue al usuario
             y_bt.style.zIndex = '9999';    // Se asegura de estar sobre todo
             y_bt.style.top = '0'; 
